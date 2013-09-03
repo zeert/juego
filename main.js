5a5c2ad9f41e3f1a869e4539500be4737cef2775
Reinaldo Zurita <reinaldo@zetalabs.cl>
2013-09-02 22:24:45 -0400

    Aparecen 2 ocitos en Pantalla

--- main.js
+++ main.js
@@ -1,146 +1,33 @@
 enchant();
 
-/*
- * window.onload
- *
- * The function which will be executed after loading page.
- * Command in enchant.js such as "new Core();" will cause an error if executed before entire page is loaded.
- *
- * ページがロードされた際に実行される関数。
- * すべての処理はページがロードされてから行うため、 window.onload の中で実行する。
- * 特に new Core(); は、<body> タグが存在しないとエラーになるので注意。
- */
+var BearSprite = enchant.Class.create(enchant.Sprite,{
+    initialize: function(y,imageOffset) {
+        enchant.Sprite.call(this,32,32);
+        var game = enchant.Game.instance;
+        this.x = 0;
+        this.y = y;
+        this.imageOffset = imageOffset;
+        this.ageDivisor = game.fps/15;
+        this.image = game.assets["chara1.png"];
+        game.rootScene.addChild(this);
+    },
+    onenterframe: function(e) {
+        this.frame = (this.age/this.ageDivisor) % 2 + this.imageOffset;
+    }
+});
+
 window.onload = function(){
-    /**
-     * new Core(width, height)
-     *
-     * Make instance of enchant.Core class. Set window size to 320 x 320
-     * Core オブジェクトを作成する。
-     * 画面の大きさは 320ピクセル x 320ピクセル に設定する。
-     */
-    var game = new Core(320, 320);
-
-    /**
-     * Core.fps
-     *
-     * Set fps (frame per second) in this game to 15.
-     * ゲームの fps (frame per second) を指定する。この場合、1秒間あたり15回画面が更新される。
-     */
-    game.fps = 15;
-    /**
-     * Core#preload
-     *
-     * You can preload all assets files before starting the game.
-     * Set needed file lists in relative/absolute path for attributes of Core#preload
-     * 必要なファイルを相対パスで引数に指定する。 ファイルはすべて、ゲームが始まる前にロードされる。
-     */
+    var game = new Game(320, 320);
+    game.fps = 30;
     game.preload("chara1.png");
-
-    /**
-     * Core#onload
-     *
-     * ロードが完了した直後に実行される関数を指定している。
-     * onload プロパティは load イベントのリスナとして働くので、以下の2つの書き方は同じ意味。
-     *
-     * game.onload = function(){
-     *     // code
-     * }
-     *
-     * game.addEventListener("load", function(){
-     *     // code
-     * })
-     */
     game.onload = function(){
-        /**
-         * new Sprite(width, height)
-         * スプライトオブジェクトを作成する。
-         * Sprite は、Entity, Node, EventTarget を継承しており、それぞれのメソッドやプロパティを使うことができる。
-         */
-        oso = new Sprite(32, 32);
+        var frameBasedBear = new BearSprite(0,6);
+        var timeBasedBear = new BearSprite(30,26);
 
-        /**
-         * Sprite.image {Object}
-         * Core#preload で指定されたファイルは、Core.assets のプロパティとして格納される。
-         * Sprite.image にこれを代入することで、画像を表示することができる
-         */
-        oso.image = game.assets["chara1.png"];
+        frameBasedBear.tl.moveTo(200,frameBasedBear.y,200).scaleTo(-1,1,1).moveTo(0,frameBasedBear.y,200).scaleTo(1,1,1).loop();
 
-        /**
-         * Node.x Node.y {Number}
-         * x, y 座標を指定する。
-         * viewport の大きさに合わせて画面が拡大縮小されている場合も、
-         * オリジナルの座標系で指定できる。
-         */
-        oso.x = 0;
-        oso.y = 0;
-
-        /**
-         * Sprite.frame {Number}
-         * (width, height) ピクセルの格子で指定された画像を区切り、
-         * 左上から数えて frame 番目の画像を表示することができる。
-         * デフォルトでは、0:左上の画像が表示される。
-         * このサンプルでは、シロクマが立っている画像を表示する (chara1.gif 参照)。
-         */
-        oso.frame = 5;
-        /**
-         * Group#addChild(node) {Function}
-         * オブジェクトをノードツリーに追加するメソッド。
-         * ここでは、クマの画像を表示するスプライトオブジェクトを、rootScene に追加している。
-         * Core.rootScene は Group を継承した Scene クラスのインスタンスで、描画ツリーのルートになる特別な Scene オブジェクト。
-         * この rootScene に描画したいオブジェクトを子として追加する (addChild) ことで、毎フレーム描画されるようになる。
-         * 引数には enchant.Node を継承したクラス (Entity, Group, Scene, Label, Sprite..) を指定する。
-         */
-        game.rootScene.addChild(oso);
-
-        /**
-         * EventTarget#addEventListener(event, listener)
-         * イベントに対するリスナを登録する。
-         * リスナとして登録された関数は、指定されたイベントの発行時に実行される。
-         * よく使うイベントには、以下のようなものがある。
-         * - "touchstart" : タッチ/クリックされたとき
-         * - "touchmove" : タッチ座標が動いた/ドラッグされたとき
-         * - "touchend" : タッチ/クリックが離されたとき
-         * - "enterframe" : 新しいフレームが描画される前
-         * - "exitframe" : 新しいフレームが描画された後
-         * enchant.js やプラグインに組み込まれたイベントは、それぞれのタイミングで自動で発行されるが、
-         * EventTarget#dispatchEvent で任意のイベントを発行することもできる。
-         *
-         * ここでは、右に向かって走っていくアニメーションを表現するために、
-         * 新しいフレームが描画される前に、毎回クマの画像を切り替え、x座標を1増やすという処理をリスナとして追加する。
-         */
-        oso.addEventListener("enterframe", function(){
-            /**
-             * クマを走らせるために、x座標をインクリメントしている。
-             * この無名関数 function(){ ... } は enterframe イベントのリスナなので、毎フレーム実行される。
-             */
-            this.x += 1;
-
-            /**
-             * this.age (Node.age) は、クマのオブジェクトが今までに何回描画されたか
-             * クマの画像を変えて走るアニメーションを表現するために、
-             * frame を 6 -> 7 -> 6 -> 7.. と順番に変えている。
-             */
-            this.frame = this.age % 2 + 6;
-        });
-
-        /**
-         * タッチされると消える処理を実現するために、
-         * touchstart イベントが起こったとき、クマが消える処理をリスナとして追加する。
-         */
-        oso.addEventListener("touchstart", function(){
-            /**
-             * クマを game.rootScene から削除する。
-             * Group#addChild の逆は Group#removeChild。
-             */
-            game.rootScene.removeChild(oso);
-        });
+        timeBasedBear.tl.setTimeBased();
+        timeBasedBear.tl.moveTo(200,timeBasedBear.y,(200/game.fps)*1000).scaleTo(-1,1,1).moveTo(0,timeBasedBear.y,(200/game.fps)*1000).scaleTo(1,1,1).loop();
     };
-
-    /**
-     * Core#start
-     * ゲームを開始する。この関数を実行するまで、ゲームは待機状態となる。
-     * 代わりに Core#debug を使うことで、デバッグモードで起動することができる。
-     * Core#pause(); で一時停止し、 Core#resume(); で再開することができる。
-     */
     game.start();
 };
